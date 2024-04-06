772d3a8714bd73ce0e7e1c5c700b090d232514c5
Kelvin Bentes <kelvinbentes20@gmail.com>
2024-04-05 20:26:00 -0400

    Altura e Idade
    Foi criado um programa que ler a idade e a altura do usuário, e imprime na tela.

---
+++ Altura_Idade.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int idade;
+    double altura;
+
+    cin >> idade;
+    cin >> altura;
+
+    cout << "Idade: " <<idade <<endl;
+    cout << "Altura: " << altura << endl;
+    return 0;
+}
